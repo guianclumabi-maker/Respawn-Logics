be8040d79dd19abbcf98ab6ac94c2fb6b8b9d3d0
Guian <guian@example.com>
2026-06-20 23:11:47 +0800

    chore: cleanup legacy frontend directories and php files

--- pages/esm_admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-$supportMode = 'esm';
-$supportRole = 'agent';
-$pageTitle = 'Employee Service Mgmt - HR/Admin Inbox';
-require __DIR__ . '/support_app_loader.php';
--- pages/esm_employee.php
+++
@@ -1,5 +0,0 @@
-<?php
-$supportMode = 'esm';
-$supportRole = 'client';
-$pageTitle = 'IT / HR Service Desk - Employee View';
-require __DIR__ . '/support_app_loader.php';
